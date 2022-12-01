fc6c7f47fe00da370796a772f992a8cc3288ac73
Miles Bardon <milesbardon@gmail.com>
2022-12-01 08:36:16 +0000

    Finishing Day 1

--- src/day1/main.test.ts
+++ src/day1/main.test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.161.0/testing/asserts.ts";
 import { describe, it } from "https://deno.land/std@0.161.0/testing/bdd.ts";
 
-import { sumCalories } from "./main.ts";
+import { sumCalories, runPart1, part1, runPart2, part2 } from "./main.ts";
 
 describe("calculates total calories", () => {
   it("should calculate total calories for a single line", () => {
@@ -10,5 +10,57 @@ describe("calculates total calories", () => {
 
   it("should calculate total calories for multiple lines", () => {
     assertEquals(sumCalories([1000, 2000, 3000]), 6000);
+  });
+});
+
+describe("example input", () => [
+  it("should calculate the greatest number of calories an elf is carrying", () => {
+    const input = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+    assertEquals(runPart1(input), 24000);
+  }),
+
+  it("should calculate the total calories the top 3 elves are carrying", () => {
+    const input = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+    assertEquals(runPart2(input), 45000);
+  }),
+]);
+
+describe("Part 1", () => {
+  it("should return the correct answer", () => {
+    assertEquals(part1(), 67658);
+  });
+});
+
+describe("Part 2", () => {
+  it("should return the correct answer", () => {
+    assertEquals(part2(), 200158);
   });
 });
--- src/day1/main.ts
+++ src/day1/main.ts
@@ -1,2 +1,59 @@
 export const sumCalories = (rations: number[]) =>
   rations.reduce((acc, n) => acc + n, 0);
+
+export const runPart1 = (input: string) => {
+  const lines = input.split("\n");
+
+  const backpacks: number[][] = [];
+  let currentBackpack: number[] = [];
+
+  lines.forEach((v) => {
+    if (v === "") {
+      backpacks.push(currentBackpack);
+      currentBackpack = [];
+    } else {
+      currentBackpack.push(parseInt(v));
+    }
+  });
+
+  return backpacks.map(sumCalories).sort((a, b) => b - a)[0];
+};
+
+export const runPart2 = (input: string) => {
+  const lines = input.split("\n");
+
+  const backpacks: number[][] = [];
+  let currentBackpack: number[] = [];
+
+  lines.forEach((v, i) => {
+    if (v === "") {
+      backpacks.push(currentBackpack);
+      currentBackpack = [];
+    } else {
+      currentBackpack.push(parseInt(v));
+    }
+
+    if (i === lines.length - 1) {
+      backpacks.push(currentBackpack);
+    }
+  });
+
+  return sumCalories(
+    backpacks
+      .map(sumCalories)
+      .sort((a, b) => b - a)
+      .slice(0, 3)
+  );
+};
+
+export const part1 = () => {
+  const input = Deno.readTextFileSync("./src/day1/input.txt");
+
+  return runPart1(input);
+};
+
+export const part2 = () => {
+  const input = Deno.readTextFileSync("./src/day1/input.txt");
+
+  return runPart2(input);
+};
