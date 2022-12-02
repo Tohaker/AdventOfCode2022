d2ba3a0787b37b55e95318df6ba0ed86e099c6c1
Miles Bardon <milesbardon@gmail.com>
2022-12-02 23:13:59 +0000

    Speeding up part 2

--- src/day2/main.ts
+++ src/day2/main.ts
@@ -1,94 +1,30 @@
-enum Opponent {
-  ROCK = "A",
-  PAPER = "B",
-  SCISSORS = "C",
-}
-
-enum Player {
-  ROCK = "X",
-  PAPER = "Y",
-  SCISSORS = "Z",
-}
-
-enum PlayerScore {
-  ROCK = 1,
-  PAPER,
-  SCISSORS,
-}
-
-enum Outcome {
-  WIN = 6,
-  DRAW = 3,
-  LOSE = 0,
-}
-
-enum Expectation {
-  LOSE = "X",
-  DRAW = "Y",
-  WIN = "Z",
-}
-
-export const calculateScore = (game: string) => {
-  const opp = game.trim().charAt(0);
-  const pla = game.trim().charAt(2);
-
-  if (opp === Opponent.ROCK) {
-    if (pla === Player.ROCK) {
-      return PlayerScore.ROCK + Outcome.DRAW;
-    } else if (pla === Player.PAPER) {
-      return PlayerScore.PAPER + Outcome.WIN;
-    } else {
-      return PlayerScore.SCISSORS + Outcome.LOSE;
-    }
-  } else if (opp === Opponent.PAPER) {
-    if (pla === Player.ROCK) {
-      return PlayerScore.ROCK + Outcome.LOSE;
-    } else if (pla === Player.PAPER) {
-      return PlayerScore.PAPER + Outcome.DRAW;
-    } else {
-      return PlayerScore.SCISSORS + Outcome.WIN;
-    }
-  } else {
-    if (pla === Player.ROCK) {
-      return PlayerScore.ROCK + Outcome.WIN;
-    } else if (pla === Player.PAPER) {
-      return PlayerScore.PAPER + Outcome.LOSE;
-    } else {
-      return PlayerScore.SCISSORS + Outcome.DRAW;
-    }
-  }
+const scoreMap: Record<string, number> = {
+  "A X": 4,
+  "A Y": 8,
+  "A Z": 3,
+  "B X": 1,
+  "B Y": 5,
+  "B Z": 9,
+  "C X": 7,
+  "C Y": 2,
+  "C Z": 6,
 };
 
-export const calculateStrategy = (game: string) => {
-  const opp = game.trim().charAt(0);
-  const pla = game.trim().charAt(2);
-
-  if (opp === Opponent.ROCK) {
-    if (pla === Expectation.WIN) {
-      return PlayerScore.PAPER + Outcome.WIN;
-    } else if (pla === Expectation.DRAW) {
-      return PlayerScore.ROCK + Outcome.DRAW;
-    } else {
-      return PlayerScore.SCISSORS + Outcome.LOSE;
-    }
-  } else if (opp === Opponent.PAPER) {
-    if (pla === Expectation.WIN) {
-      return PlayerScore.SCISSORS + Outcome.WIN;
-    } else if (pla === Expectation.DRAW) {
-      return PlayerScore.PAPER + Outcome.DRAW;
-    } else {
-      return PlayerScore.ROCK + Outcome.LOSE;
-    }
-  } else {
-    if (pla === Expectation.WIN) {
-      return PlayerScore.ROCK + Outcome.WIN;
-    } else if (pla === Expectation.DRAW) {
-      return PlayerScore.SCISSORS + Outcome.DRAW;
-    } else {
-      return PlayerScore.PAPER + Outcome.LOSE;
-    }
-  }
+const strategyMap: Record<string, number> = {
+  "A X": 3,
+  "A Y": 4,
+  "A Z": 8,
+  "B X": 1,
+  "B Y": 5,
+  "B Z": 9,
+  "C X": 2,
+  "C Y": 6,
+  "C Z": 7,
 };
+
+export const calculateScore = (game: string) => scoreMap[game.trim()];
+
+export const calculateStrategy = (game: string) => strategyMap[game.trim()];
 
 export const runPart1 = (input: string) =>
   input.split("\n").reduce((acc, l) => acc + calculateScore(l), 0);
